4749282508dfe235c004bbbc06778cb9b1abd182
Charles Southerland <charlie@stuphlabs.com>
2014-08-25 16:52:46 -0500

    Initial checkin

---
+++ src/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Content-type: text/html\n\n");
+	printf("<html><head><title>Hello CGI</title></head>");
+	printf("<body><h1>Hello CGI</h1></body></html>");
+
+	return 0;
+}
+
